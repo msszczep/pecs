6a65ea3889330f7d90796c7a93633f0a5ef786d5
Mitchell Szczepanczyk <mitchell@szcz.org>
2020-08-06 21:25:39 -0500

    Using elm-format

--- Pecs.elm
+++ Pecs.elm
@@ -1,70 +1,87 @@
+module Main exposing (..)
+
 import Browser
 import Html exposing (Html, button, div, text, p)
--- import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Debug exposing (toString)
 
+
+-- import Html.Attributes exposing (..)
 -- MAIN
 
+
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
+
 
 -- MODEL
 
-type alias Actor = 
-  { id : Int
-  , name : String
-  , cc : Int
-  , wc : Int
-  , numBeersWanted : Float
-  , numPizzasWanted : Float
-  , consumptionThreshold : Float
-  , hoursToWork : Float
-  , maxLeisureTime : Float
-  }
+
+type alias Actor =
+    { id : Int
+    , name : String
+    , cc : Int
+    , wc : Int
+    , numBeersWanted : Float
+    , numPizzasWanted : Float
+    , consumptionThreshold : Float
+    , hoursToWork : Float
+    , maxLeisureTime : Float
+    }
+
 
 type alias Prices =
-  { pizza : Float
-  , beer : Float
-  }
+    { pizza : Float
+    , beer : Float
+    }
 
-type alias Model = 
-  { actors : List Actor
-  , prices : Prices
-  } 
+
+type alias Model =
+    { actors : List Actor
+    , prices : Prices
+    }
+
 
 newPriceVector : Prices
-newPriceVector = Prices 10.0 10.0
+newPriceVector =
+    Prices 10.0 10.0
+
 
 init : Model
 init =
-  Model [] newPriceVector
+    Model [] newPriceVector
+
 
 newActor : Actor
 newActor =
-  Actor 0 "" 0 0 0.0 0.0 0.0 0.0 0.0
+    Actor 0 "" 0 0 0.0 0.0 0.0 0.0 0.0
+
+
 
 -- UPDATE
 
+
 type Msg
-  = AddActor Actor
+    = AddActor Actor
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    AddActor a ->
-      { model | actors = a :: model.actors }
+    case msg of
+        AddActor a ->
+            { model | actors = a :: model.actors }
+
+
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div [] 
-      [ 
-        button
-        [ onClick (AddActor newActor) ]
-        [ text "Add Actor" ]
-      , p [] [ text (toString model) ]
-      ]
-
-
+    div []
+        [ button
+            [ onClick (AddActor newActor) ]
+            [ text "Add Actor" ]
+        , p [] [ text (toString model) ]
+        ]
